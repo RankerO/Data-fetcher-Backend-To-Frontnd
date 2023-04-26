cb0320022bed589fefb3bd7d2a31632349a524d4
sam pandey <shamikantpandey559@gmail.com>
2023-04-27 04:22:30 +0530

    fetching data and displaying on conditions completed

---
+++ back_end/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const cors = require("cors");
+const path = require("path");
+const bodyParser = require("body-parser");
+const app = express();
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const userRoutes  = require("./routes/userRoutes");
+const connectDB = require("./db/connect");
+app.use(cors());
+dotenv.config();
+connectDB();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api", userRoutes);
+
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
+
+
+// Static files
+app.use(express.static(path.join(__dirname, './front_end/build')));
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, './front_end/build/index.html'));
+})
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`Server Started on PORT ${PORT}`.yellow.bold);
+});
+
